dce78f1ad7aba141703d0cd66e04d85bb211f091
Mike Lee <rockespecialee@gmail.com>
2025-01-13 11:22:23 -0800

    expand zip files

---
+++ 2DGamePhysicsEngineProgramming/14-ParticleIntegrateFunction/2dphysics/src/Physics/Particle.h
@@ -0,0 +1,21 @@
+#ifndef PARTICLE_H
+#define PARTICLE_H
+
+#include "Vec2.h"
+
+struct Particle {
+    int radius;
+
+    Vec2 position;
+    Vec2 velocity;
+    Vec2 acceleration;
+
+    float mass;
+
+    Particle(float x, float y, float mass);
+    ~Particle();
+
+    void Integrate(float dt);
+};
+
+#endif
